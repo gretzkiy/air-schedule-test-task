ad8554b1c994621ff3d2fc5ad924e2282378325c
Andrey Dikarev <dias7707@gmail.com>
2019-07-05 01:16:35 +0300

    - update docker-compose

--- server/server.js
+++ server/server.js
@@ -1,62 +1,70 @@
 const express = require('express');
 const fallback = require('express-history-api-fallback');
-const https = require('https');
+// const proxy = require('express-http-proxy');
 const config = require('./config.js');
 
 const app = express();
 const rootDir = `${__dirname}/..`;
 
+// app.use((req, res, next) => {
+//     console.log(req.url);
+//     console.log(res);
+//     next();
+// });
+
 app.use(express.static(`${rootDir}/dist`));
-app.use('/api',  (request, response) => {
-    if (request.query.event !== 'departure' && request.query.event !== 'arrival') {        
-        response.status(400).send('Неверный запрос.');
-        return;
-    }
+// app.use('/api',  (request, response) => {
+//     if (request.query.event !== 'departure' && request.query.event !== 'arrival') {        
+//         response.status(400).send('Неверный запрос.');
+//         return;
+//     }
 
-    const event = request.query.event;
-    const now = new Date();
-    const today = now.toISOString().split('T')[0];
+//     const event = request.query.event;
+//     const now = new Date();
+//     const today = now.toISOString().split('T')[0];
 
-    const url = `https://api.rasp.yandex.net/v3.0/schedule/?apikey=${config.apikey}&station=svo&lang=ru_RU&format=json&date=${today}&transport_types=plane&event=${event}&system=iata`;
+//     const url = `https://api.rasp.yandex.net/v3.0/schedule/?apikey=${config.apikey}&station=svo&lang=ru_RU&format=json&date=${today}&transport_types=plane&event=${event}&system=iata`;
 
-    console.log('Making request to ', url);
+//     console.log('Making request to ', url);
     
-    let schedule = null;
-    https.get(url, (res) => {
-        const { statusCode } = res;
-        const contentType = res.headers['content-type'];
+//     let schedule = null;
+//     https.get(url, (res) => {
+//         const { statusCode } = res;
+//         const contentType = res.headers['content-type'];
 
-        let error;
-        if (statusCode !== 200) {
-            error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-            error = new Error('Invalid content-type.\n' +
-                `Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-            console.error(error.message);
-            res.resume();
-            response.status(500).send('Не удается получить расписание.');
-            return;
-        }
+//         let error;
+//         if (statusCode !== 200) {
+//             error = new Error('Request Failed.\n' +
+//                 `Status Code: ${statusCode}`);
+//         } else if (!/^application\/json/.test(contentType)) {
+//             error = new Error('Invalid content-type.\n' +
+//                 `Expected application/json but received ${contentType}`);
+//         }
+//         if (error) {
+//             console.error(error.message);
+//             res.resume();
+//             response.status(500).send('Не удается получить расписание.');
+//             return;
+//         }
 
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            try {
-                schedule = JSON.parse(rawData);
-                response.json(schedule);
-            } catch (e) {
-                console.error(e.message);
-            }
-        });
-    }).on('error', (e) => {
-        console.error(`Got error: ${e.message}`);
-        response.status(500).send('Не удается получить расписание.');
-    });
-});
+//         res.setEncoding('utf8');
+//         let rawData = '';
+//         res.on('data', (chunk) => { rawData += chunk; });
+//         res.on('end', () => {
+//             try {
+//                 schedule = JSON.parse(rawData);
+//                 response.json(schedule);
+//             } catch (e) {
+//                 console.error(e.message);
+//             }
+//         });
+//     }).on('error', (e) => {
+//         console.error(`Got error: ${e.message}`);
+//         response.status(500).send('Не удается получить расписание.');
+//     });
+// });
+
+// app.use('/api', proxy('http://localhost:4000'));
 
 app.use(fallback('dist/index.html', { root: rootDir }));
 
